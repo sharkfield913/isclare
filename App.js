98dc8cc964510d4dd2c12f8442783387e18543b4
sharkfield913 <oowgnas@naver.com>
2023-12-15 00:05:47 +0900

    gmap

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
-import React, {Component} from 'react';
-import {requireNativeComponent, Text, View} from 'react-native';
+import React from 'react';
+import { requireNativeComponent, View, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-const TMap = requireNativeComponent("TMap")
+//const TMap = requireNativeComponent('TMap', null);
 
-export default class App extends Component {
-  
-  render() {
-    return (
-      <View style={{flex:1}}> 
-        <TMap
-          style={{flex: 1}}
-          setCenter={[127.0, 37.0]}
-        />
-      </View>
-    );
-  }
+function App() {
+  return (
+    <>
+    <View style={{ flex: 1 }}>
+      {/* <TMap style={{ flex: 1 }} setCenter={[127.0, 37.0]} /> */}
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37,
+          longitude: 127,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }} />
+    </View>
+    </>
+  );
 }
+
+export default App;
